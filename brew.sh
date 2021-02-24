88db46d0a22d6bf6e29e9acaf4e836c42f0fae74
ulrikah <ulrikhalmoy@gmail.com>
2021-02-24 13:31:41 +0100

    Add some brew casks and formulae

---
+++ brew.sh
@@ -0,0 +1,33 @@
+if ! command -v brew &> /dev/null
+then
+    echo "brew not found. Installing it now 🍺"
+    curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh
+fi
+
+# formulae
+brew install git
+brew install jq # json
+brew install tmux
+brew install neovim
+brew install wget
+
+brew install tree # tree view of dirs
+brew install fzf # fuzzy search
+brew install fd # alternative to find
+brew install tldr # alternative to man pages
+brew install bat # prettier cat
+
+brew install node
+brew install pyenv # python version manager
+brew install rbenv # ruby version manager
+brew install docker
+brew install postgresql
+
+brew install ffmpeg # media editing
+brew install sox # audio processing
+brew install imagemagick # image processing
+
+# casks
+brew install --cask rectangle
+brew install --cask blackhole
+
