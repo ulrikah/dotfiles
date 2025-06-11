45fa0fce30bd22f59f3a6b6154d490330b43aebc
Ulrik Antoniussen Halmøy <ulrikhalmoy@gmail.com>
2025-06-11 13:49:39 +0200

    retab

--- install.sh
+++ install.sh
@@ -6,20 +6,20 @@ cd "$(dirname "${BASH_SOURCE}")";
 
 if [[ $* == *--pull* ]]
 then
-	echo "Pulling dotfiles origin";
-	git pull origin main
+    echo "Pulling dotfiles origin";
+    git pull origin main
 fi
 
 function doIt() {
-	rsync --exclude ".git/" \
-		--exclude ".DS_Store" \
-		--exclude "install.sh" \
-		--exclude "README.md" \
-		--exclude "fonts" \
-		--exclude "brew/" \
-		--exclude "iterm/" \
-		-avh --no-perms . ~;
-	source ~/.zshrc;
+    rsync --exclude ".git/" \
+        --exclude ".DS_Store" \
+        --exclude "install.sh" \
+        --exclude "README.md" \
+        --exclude "fonts" \
+        --exclude "brew/" \
+        --exclude "iterm/" \
+        -avh --no-perms . ~;
+    source ~/.zshrc;
 }
 
 doIt;
